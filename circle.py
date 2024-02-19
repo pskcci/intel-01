25acbdbfbb607ea38b6c07358ce839f7e4acc7e4
brkim92 <kbr921123@gmail.com>
2024-02-19 13:59:28 +0900

    uploadfile

---
+++ circle.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(4)
+
+width, height = 640, 480
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    if ret is False:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    edges = cv2.Canny(gray, 50, 150)
+    
+    cv2.imshow("Edges", edges)
+
+    lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+    if lines is not None:
+        for line in lines:
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50,
+                               param1=120, param2=30, minRadius=0, maxRadius=0)
+    
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+
+    cv2.imshow("Camera", frame)
+    
+
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
